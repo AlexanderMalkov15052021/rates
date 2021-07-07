e5f8063d87511477ce30b2f7c913a9b8ca737d86
AlexanderMalkov15052021 <84239559+AlexanderMalkov15052021@users.noreply.github.com>
2021-07-07 07:28:01 +0300

    Changed
    Changed

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const staticCacheName = 's-app-v1'
-const dynamicCacheName = 'd-app-v1'
+const staticCacheName = 's-app-v3'
+const dynamicCacheName = 'd-app-v3'
 
 const assetUrls = [
   'index.html',
-  'app.js',
-  'index.css',
+  'sw.js',
+  '/js/app.js',
+  '/css/styles.css',
   'offline.html'
 ]
 
