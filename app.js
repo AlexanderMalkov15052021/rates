38584f6edab3c84b7aeb2349cec33a50fc08ee43
AlexanderMalkov15052021 <84239559+AlexanderMalkov15052021@users.noreply.github.com>
2021-07-07 07:49:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
       const reg = await navigator.serviceWorker.register('sw.js')
-      console.log('Service worker register success', reg)
+      console.log('Service worker register success')
     } catch (e) {
       console.log('Service worker register fail')
     }
