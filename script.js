42c7a5ac6f790f29068a7fcea8f20743b6d0aa5d
AlexanderMalkov15052021 <84239559+AlexanderMalkov15052021@users.noreply.github.com>
2021-07-10 13:37:45 +0300

    Changes files
    Changes files

--- script.js
+++ script.js
@@ -130,7 +130,21 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+  // showInstallPromotion();
   // Optionally, send analytics event that PWA install promo was shown.
   console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+buttonInstall.addEventListener('click', async () => {
+  console.log(1);
+  // Hide the app provided install promotion
+  // hideInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
 });
