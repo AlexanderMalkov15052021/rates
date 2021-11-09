2096ecdc0f290df5ca7f75f8a0dc36155e821cc7
AlexanderMalkov15052021 <84239559+AlexanderMalkov15052021@users.noreply.github.com>
2021-11-09 07:12:01 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ window.addEventListener('load', async () => {
 
 async function loadPosts(e) {
   const res = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-  const res = await fetch('json.js');
+  // const res = await fetch('json.js');
   const data = await res.json()
 
   const container = document.querySelector('#posts')
